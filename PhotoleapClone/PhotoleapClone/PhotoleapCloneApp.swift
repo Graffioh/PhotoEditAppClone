75b7d9e7a5653b616f97d9c6bb06bdebc300406f
Graffioh <bregliascuola2002@gmail.com>
2022-11-15 14:59:11 +0100

    Add project

---
+++ PhotoleapClone/PhotoleapClone/PhotoleapCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoleapCloneApp.swift
+//  PhotoleapClone
+//
+//  Created by Umberto Breglia on 15/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PhotoleapCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
